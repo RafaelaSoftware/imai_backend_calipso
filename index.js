0b7e4bd23326af23f68a584e695e78d9a600ff36
Nicolas Nocete <nnocete@imai.intranet>
2024-02-21 12:09:28 -0300

    add endpoints

---
+++ index.js
@@ -0,0 +1,38 @@
+
+const express = require('express')
+var cors = require('cors')
+const db = require('./queries')
+
+const bodyParser = require('body-parser')
+const app = express()
+const port = 25002
+
+app.use(
+    cors({
+      origin: "*",
+      methods: ["GET"],
+    })
+  );
+  
+
+app.use(bodyParser.json())
+app.use(
+    bodyParser.urlencoded({
+        extended: true,
+    })
+)
+
+app.get('/', (request, response) => {
+    response.json({ info: 'Node.js, Express, and Postgres API' })
+})
+
+
+app.get('/empleado/:codigo', db.getEmpleadoByCodigo)
+app.get('/producto/:codigo', db.getProductoByCodigo)
+app.get('/ordenProduccion/:codigo', db.getOrdenProduccionByCodigo)
+app.get('/tarea/:codigo', db.getTareaByCodigo)
+
+
+app.listen(port, () => {
+    console.log(`App running on port ${port}.`)
+})
---
+++ queries.js
@@ -0,0 +1,65 @@
+const Pool = require('pg').Pool
+const pool = new Pool({
+    host: '10.34.0.40',
+    port: 5432,
+    user: 'postgres',
+    password: 'calipsodb',
+    database: 'db_calipso_imai',
+})
+
+const getEmpleadoByCodigo = (request, response) => {
+    const codigo = request.params.codigo
+    const query = `SELECT codigo, descripcion FROM v_empleado WHERE activestatus = 0 and codigo = '${codigo}'`;
+    console.log(query);
+
+    pool.query(query, (error, results) => {
+        if (error) {
+            throw error
+        }
+        response.status(200).json(results.rows)
+    })
+}
+
+const getProductoByCodigo = (request, response) => {
+    const codigo = request.params.codigo
+    const query = `SELECT codigo, descripcion FROM v_producto WHERE activestatus = 0 and codigo = '${codigo}'`;
+
+    pool.query(query, (error, results) => {
+        if (error) {
+            throw error
+        }
+        response.status(200).json(results.rows)
+    })
+}
+
+const getOrdenProduccionByCodigo = (request, response) => {
+    const codigo = request.params.codigo
+    const query = `SELECT numerodocumento as codigo, nombre as descripcion FROM v_trsolicitud WHERE tipotransaccion_id = '{EDC2BC0F-F9CF-4D74-8742-CA8A2B7A264E}' and numerodocumento = '${codigo}'`;
+
+    pool.query(query, (error, results) => {
+        if (error) {
+            throw error
+        }
+        response.status(200).json(results.rows)
+    })
+}
+
+const getTareaByCodigo = (request, response) => {
+    const codigo = request.params.codigo
+    const query = `SELECT codigo, descripcion FROM v_servicio WHERE activestatus = 0 and codigo = '${codigo}'`;
+
+    pool.query(query, (error, results) => {
+        if (error) {
+            throw error
+        }
+        response.status(200).json(results.rows)
+    })
+}
+
+
+module.exports = {
+    getEmpleadoByCodigo,
+    getProductoByCodigo,
+    getOrdenProduccionByCodigo,
+    getTareaByCodigo,
+}
