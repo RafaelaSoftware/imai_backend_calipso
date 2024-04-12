7ce1f90510fca569cc09844207190b8c642b4298
Nicolas Nocete <nicolasnocete@gmail.com>
2024-04-12 15:08:19 -0300

    add dockerfile

--- index.js
+++ index.js
@@ -1,38 +1,34 @@
+const express = require("express");
+var cors = require("cors");
+const db = require("./queries");
 
-const express = require('express')
-var cors = require('cors')
-const db = require('./queries')
-
-const bodyParser = require('body-parser')
-const app = express()
-const port = 25002
+const bodyParser = require("body-parser");
+const app = express();
+const port = 3001;
 
 app.use(
-    cors({
-      origin: "*",
-      methods: ["GET"],
-    })
-  );
-  
+  cors({
+    origin: "*",
+    methods: ["GET"],
+  })
+);
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 app.use(
-    bodyParser.urlencoded({
-        extended: true,
-    })
-)
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
 
-app.get('/', (request, response) => {
-    response.json({ info: 'Node.js, Express, and Postgres API' })
-})
+app.get("/", (request, response) => {
+  response.json({ info: "Node.js, Express, and Postgres API" });
+});
 
-
-app.get('/empleado/:codigo', db.getEmpleadoByCodigo)
-app.get('/producto/:codigo', db.getProductoByCodigo)
-app.get('/ordenProduccion/:codigo', db.getOrdenProduccionByCodigo)
-app.get('/tarea/:codigo', db.getTareaByCodigo)
-
+app.get("/empleado/:codigo", db.getEmpleadoByCodigo);
+app.get("/producto/:codigo", db.getProductoByCodigo);
+app.get("/ordenProduccion/:codigo", db.getOrdenProduccionByCodigo);
+app.get("/tarea/:codigo", db.getTareaByCodigo);
 
 app.listen(port, () => {
-    console.log(`App running on port ${port}.`)
-})
+  console.log(`App running on port ${port}.`);
+});
